5d9779349d5931539da6932532af5878be008c96
giveonaldo <giveonaldo@gmail.com>
2024-11-11 13:22:18 +0700

    add layout with ejs template

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express')
+const expressLayout = require('express-ejs-layouts')
 const app = express()
+const db = require("./database/db.js");
 const port = process.env.PORT
 
+app.use(expressLayout);
 app.use(express.json());
 // app.use('/todos', require('./routes/todos.js'))
 app.use('/tododb', require('./routes/tododb.js'))
@@ -10,12 +13,29 @@ app.set('view engine', 'ejs');
 
 // Routes to HomePage
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        layout: 'layouts/main',
+        title: "HomePage"
+    });
 });
 
 // Routes to Contact Us
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact',{
+        layout: 'layouts/main',
+        title: "contact Page"
+    });
+});
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main',
+            todos: todos,
+            title: "Todos"
+        });
+    });
 });
 
 // Routes for page not found
